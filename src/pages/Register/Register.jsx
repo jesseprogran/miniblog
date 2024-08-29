3fe03b854d72b746badf3a8251019109d5a5f0bd
Jessé <jessepescador22@gmail.com>
2024-08-29 20:14:08 -0300

    corrigindo erro no form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -27,9 +27,7 @@ const Register = () => {
     <div>
       <h1>Cadastre-se para postar</h1>
       <p>Crie seu usuário e compartilhe suas histórias</p>
-      <form>
-        {" "}
-        onSubmit={handleSubmit}
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Nome:</span>
           <input
