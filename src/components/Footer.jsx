7f3feb2dc514ffe7bcc0ad687fabef8564494adc
Jessé <jessepescador22@gmail.com>
2024-08-28 21:09:49 -0300

    realizando estilização

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Escreva Sobre o que você tem interesse!</h3>
       <p>Jessé Code &copy; 2024</p>
     </footer>
