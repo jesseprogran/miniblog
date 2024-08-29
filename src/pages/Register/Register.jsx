294dbf4b5d05c0cbbcf2c33934642b25ff68f1e6
Jessé <jessepescador22@gmail.com>
2024-08-29 19:31:49 -0300

    criando formulario de cadastro

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,54 @@
 import styles from "./Register.module.css";
 
+import { useState, useEffect } from "react";
+
 const Register = () => {
   return (
     <div>
-      <h2>Registro</h2>
+      <h1>Cadastre-se para postar</h1>
+      <p>Crie seu usuário e compartilhe suas histórias</p>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Nome do usuário"
+          />
+        </label>
+
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+          />
+        </label>
+
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Digite sua senha"
+          />
+        </label>
+
+        <label>
+          <span>Confirmação de senha:</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirme sua senha"
+          />
+        </label>
+        <button className="btn">Cadastrar</button>
+      </form>
     </div>
   );
 };
