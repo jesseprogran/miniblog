4beea4dcd875a08ab183c85c4ad9972dad66a370
Jessé <jessepescador22@gmail.com>
2024-08-30 21:49:36 -0300

    Registrando usuario ao sistema

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,13 +2,18 @@ import styles from "./Register.module.css";
 
 import { useState, useEffect } from "react";
 
+import { useAuthentication } from "../../hooks/useAuthentication";
+
 const Register = () => {
   const [displayName, setDisplayName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
-  const handleSubmit = (e) => {
+
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
 
@@ -23,8 +28,17 @@ const Register = () => {
       return;
     }
 
-    console.log(user)
+    const res = await createUser(user);
+
+    console.log(res);
   };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
   return (
     <div className={styles.register}>
       <h1>Cadastre-se para postar</h1>
@@ -74,7 +88,12 @@ const Register = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </label>
-        <button className="btn">Cadastrar</button>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde....
+          </button>
+        )}
         {error && <p className="error">{error}</p>}
       </form>
     </div>
