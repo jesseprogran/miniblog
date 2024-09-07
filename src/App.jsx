dc4e3306f4e1a78565414895f371dd5319ec3b20
Jessé <jessepescador22@gmail.com>
2024-09-07 20:11:44 -0300

    Links para usuário autenticado

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,18 @@ import { useAuthentication } from "./hooks/useAuthentication";
 // pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 // components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 
 // context
 import { AuthProvider } from "./context/AuthContext";
+
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -48,6 +50,8 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/posts/create" element={<CreatePost />} />
+              <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
           </div>
           <Footer />
