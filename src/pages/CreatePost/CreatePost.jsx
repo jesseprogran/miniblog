108008f962bfc45f6b57093bf8dd9b1e76f1e30a
Jessé <jessepescador22@gmail.com>
2024-10-10 20:43:09 -0300

    estruturando a home

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,8 +1,9 @@
 import styles from "./CreatePost.module.css";
 
 import { useState } from "react";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
 import { useNavigate } from "react-router-dom";
-import { useAsyncValue } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -11,8 +12,55 @@ const CreatePost = () => {
   const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
 
+  const { user } = useAuthValue();
+
+  const navigate = useNavigate();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError("");
+
+    // validate image
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
+
+    // create tags array
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    // check values
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    console.log(tagsArray);
+
+    console.log({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    if (formError) return;
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // redirect to home page
+    navigate("/");
   };
 
   return (
@@ -24,56 +72,54 @@ const CreatePost = () => {
           <span>Título:</span>
           <input
             type="text"
-            name="title"
+            name="text"
             required
             placeholder="Pense num bom título..."
             onChange={(e) => setTitle(e.target.value)}
             value={title}
           />
         </label>
-
         <label>
           <span>URL da imagem:</span>
           <input
             type="text"
             name="image"
             required
-            placeholder="Insira uma image que representa seu post.."
+            placeholder="Insira uma imagem que representa seu post"
             onChange={(e) => setImage(e.target.value)}
             value={image}
           />
         </label>
-
         <label>
           <span>Conteúdo:</span>
           <textarea
             name="body"
             required
-            placeholder="Insira o conteúdo do post.."
+            placeholder="Insira o conteúdo do post"
             onChange={(e) => setBody(e.target.value)}
             value={body}
           ></textarea>
         </label>
-
         <label>
           <span>Tags:</span>
           <input
             type="text"
             name="tags"
             required
-            placeholder="Insira as tags separada por vírgula"
+            placeholder="Insira as tags separadas por vírgula"
             onChange={(e) => setTags(e.target.value)}
             value={tags}
           />
         </label>
-        <button className="btn">Cadastrar</button>
-        {/* {!loading && }
-        {loading && (
+        {!response.loading && <button className="btn">Criar post!</button>}
+        {response.loading && (
           <button className="btn" disabled>
-            Aguarde....
+            Aguarde.. .
           </button>
         )}
-        {error && <p className="error">{error}</p>} */}
+        {(response.error || formError) && (
+          <p className="error">{response.error || formError}</p>
+        )}
       </form>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,30 @@
 // css
-import styles from'./Home.module.css'
+import styles from "./Home.module.css";
+
+// hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
+// components
+
 export const Home = () => {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Veja nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="ou busque por tags..." onChange={(e) => setQuery(e.target.value)}/>
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>posts..</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
