e28aaf368522200a8ed12d769c6d46b082d91f45
Jessé <jessepescador22@gmail.com>
2024-10-11 19:47:18 -0300

    hook de resgate de dados

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,9 +15,9 @@ export const Home = () => {
     e.preventDefault();
   };
   return (
-    <div>
+    <div className={styles.home}>
       <h1>Veja nossos posts mais recentes</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
         <input
           type="text"
           placeholder="ou busque por tags..."
@@ -27,6 +27,12 @@ export const Home = () => {
       </form>
       <div>
         <h1>posts..</h1>
+        {posts && posts.length === 0 &&(
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts..</p>
+            <Link to="/posts/create" className="btn">Criar primeiro post..</Link>
+          </div>
+        )}
       </div>
     </div>
   );
